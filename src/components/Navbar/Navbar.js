2cdac36ec0001b0822cfd13fc05e42510bec9c57
Vernon Lillies <vernon.lillies@gmail.com>
2018-02-18 23:21:23 -0500

    feat: add navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import logo from '../../assets/logo/ts-logo.svg'
-import './Navbar.css'
+import logo from '../../img/ts-logo.svg'
+import './Navbar.scss'
 
 const navbar = () => {
   return (
