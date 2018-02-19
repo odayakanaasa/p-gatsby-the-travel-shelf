adf819b33d10fc869e185a2e8b378e601a092d49
Vernon Lillies <vernon.lillies@gmail.com>
2018-02-19 10:28:09 -0500

    tmp work in progress
    - Can't add background images
    - shart image loader makes me crash

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import Form from '../components/EmailForm/EmailForm'
+import image from '../static/img/IMG_4702.JPG'
 
 import './index.scss'
 
 const IndexPage = () => (
 <div>
+  <img src=image />
   <section id='showcase' className='py-5 text-white'>
+
     <div className='primary-overlay'>
       <div className='container'>
         <div className='row justify-content-center'>
