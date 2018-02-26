28eb51b83bf7a2900446a5a36553ecaab7539a42
VernL <vernon.lillies@gmail.com>
2018-02-25 23:16:45 -0500

    feat: add bookstore-posts query on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,42 +3,61 @@ import Link from 'gatsby-link'
 
 import './index.scss'
 
-const IndexPage = ({data}) => (
-<div className='top'>
-  <section id='showcase' className='py-5 text-white'>
-    <div className='primary-overlay'>
-      <div className='container'>
-        <div className='row justify-content-center'>
-          <div className='col text-center'>
-            <h1 className='display-4 myhero text-center'>A magazine of beautiful independent bookstores around the world</h1>
-            <Link to='/about' className='btn btn-outline-warning btn-lg read'><i
-              className='fa fa-arrow-right' /> Read More {data.site.siteMetadata.title}</Link>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
+const IndexPage = ({ data }) => {
+  console.log(data)
+  return (
+    <div className='top'>
+        <section id='showcase' className='py-5 text-white'>
+            <div className='primary-overlay'>
+                <div className='container'>
+                    <div className='row justify-content-center'>
+                        <div className='col text-center'>
+                            <h1 className='display-4 myhero text-center'>A magazine of beautiful independent bookstores
+                                around the world</h1>
+                            <Link to='/about' className='btn btn-outline-warning btn-lg read'><i
+                                className='fa fa-arrow-right'/> Read More </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
 
-  <section id='portfolio'>
-    <div className='container-fluid'>
-      <h2 className='text-center'>Welcome To The Travel Shelf</h2>
-      <hr align='center' width='30%' />
-      <p className='text-center header'>Browse Our Portfolio And Visit Your Local Independent Bookstores Today!</p>
-      <div className='row no-gutters d-flex flex-wrap justify-content-center align-items-baseline'>
-      </div>
-    </div>
-  </section>
+        <section id='portfolio'>
+            <div className='container-fluid'>
+                <h2 className='text-center'>Welcome To The Travel Shelf</h2>
+                <hr align='center' width='30%'/>
+                <p className='text-center header'>Browse Our Portfolio And Visit Your Local Independent Bookstores
+                    Today!</p>
+                <div className='row no-gutters d-flex flex-wrap justify-content-center align-items-baseline'>
+                  {data.allMarkdownRemark.edges.map(({ node }) => (
+                  <div className='col-12'>
+                  <h3>{node.frontmatter.title}{" "}</h3>
+                  <p>{node.excerpt}</p>
+                  </div>
+                  ))}
+                </div>
+            </div>
+        </section>
 
-</div>
-)
+    </div>
+  )
+}
 
 export default IndexPage
 
 export const query = graphql`
-  query AboutQuery {
-    site {
-      siteMetadata {
-        title
+  query IndexQuery {
+    allMarkdownRemark {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          excerpt
+        }
       }
     }
   }`
+
